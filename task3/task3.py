3697bdfee56cce516f935cf22e1f2039de5045f4
Ermaviv <ermaviv@yandex.ru>
2025-05-01 15:18:25 +0300

    Fix result of report.json

--- task3/task3.py
+++ task3/task3.py
@@ -1,15 +1,18 @@
 import json
 import pandas
 
-with (open("report.json", 'w') as report_file):
-
-    tests = pandas.read_json("tests.json").to_dict()
+with (
+    open("report.json", 'w') as report_file,
+    open("tests.json", 'r') as tests_file,
+):
+    tests = json.load(tests_file)
     dic_values = pandas.read_json("values.json").to_dict()
 
     id_value = {}
 
     for test in dic_values["values"].values():
         id_value[test['id']] = test['value']
+
 
     def recursive_call(data):
         for tmp in data:
@@ -18,8 +21,7 @@ with (open("report.json", 'w') as report_file):
             if 'values' in tmp.keys():
                 recursive_call(tmp['values'])
 
-
-    tests_values = tests['tests'].values()
+    tests_values = tests['tests']
     recursive_call(tests_values)
 
     json.dump(tests, report_file, indent=2)
