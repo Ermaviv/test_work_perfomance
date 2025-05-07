a4e93bcce694490a362128195fc0e6936b986c2c
Ermaviv <ermaviv@yandex.ru>
2025-05-07 19:18:16 +0300

    Fix error in task4.py

--- task4/task4.py
+++ task4/task4.py
@@ -1,16 +1,14 @@
 import sys
 
 numbers = sys.argv[1]
-# не рабочий код, неправильное среднее число
 
 with open(numbers, 'r') as file:
     content = []
     for i in file:
         content.append(int(i))
-    mode = max(content, key=content.count)
-    if content.count(mode) == 1:
-        mode = int((max(content) + min(content)) / 2)
+    content = sorted(content)
+    median_index = int(len(content) / 2)
     steps_count = 0
     for value in content:
-        steps_count += abs(value - mode)
+        steps_count += abs(value - content[median_index])
     print(steps_count)
